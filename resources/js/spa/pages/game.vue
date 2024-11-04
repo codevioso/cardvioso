e2273164e387751685070f25ac553c21c940a182
Ridwanul Hafiz <ridwanul.hafiz@gmail.com>
2024-11-04 13:12:52 +0600

    route-setup-and-game-part-apply-in-/game

---
+++ resources/js/spa/pages/game.vue
@@ -0,0 +1,171 @@
+<template>
+
+    <div class="home">
+        <div class="card">
+            <div v-for="(each, index) in cardData" :key="index" class="card-box" @click="rotate(index)">
+                <div class="card-view" :id="'front'+index">
+                    <img :src="each.frontSrc" alt="image-front">
+                </div>
+                <div class="card-view back" :id="'back'+index">
+                    <img :src="each.backSrc" alt="image-back">
+                </div>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+
+export default {
+    data(){
+        return {
+            cardData: [
+                { id: '0', frontSrc: '/images/que_icon.svg', backSrc: '/images/img-1.png' },
+                { id: '1', frontSrc: '/images/que_icon.svg', backSrc: '/images/img-2.png' },
+                { id: '2', frontSrc: '/images/que_icon.svg', backSrc: '/images/img-3.png' },
+                { id: '3', frontSrc: '/images/que_icon.svg', backSrc: '/images/img-4.png' },
+                { id: '4', frontSrc: '/images/que_icon.svg', backSrc: '/images/img-5.png' },
+                { id: '5', frontSrc: '/images/que_icon.svg', backSrc: '/images/img-6.png' },
+                { id: '6', frontSrc: '/images/que_icon.svg', backSrc: '/images/img-7.png' },
+                { id: '7', frontSrc: '/images/que_icon.svg', backSrc: '/images/img-8.png' },
+                { id: '8', frontSrc: '/images/que_icon.svg', backSrc: '/images/img-1.png' },
+                { id: '9', frontSrc: '/images/que_icon.svg', backSrc: '/images/img-2.png' },
+                { id: '10', frontSrc: '/images/que_icon.svg', backSrc: '/images/img-3.png' },
+                { id: '11', frontSrc: '/images/que_icon.svg', backSrc: '/images/img-4.png' },
+                { id: '12', frontSrc: '/images/que_icon.svg', backSrc: '/images/img-5.png' },
+                { id: '13', frontSrc: '/images/que_icon.svg', backSrc: '/images/img-6.png' },
+                { id: '14', frontSrc: '/images/que_icon.svg', backSrc: '/images/img-7.png' },
+                { id: '15', frontSrc: '/images/que_icon.svg', backSrc: '/images/img-8.png' },
+            ],
+            firstImage: null,
+            secondImage: null,
+            lockBoard: false,
+        }
+    },
+    mounted() {
+
+    },
+    methods: {
+
+        // Function of rotate image which is inside of card
+        rotate(index) {
+
+            // if lock board true and index equal to card one then return
+            if(this.lockBoard || index === this.firstImage) return;
+
+            // take dynamic index of front card and back card
+            let frontCard = document.querySelector(`#front${index}`);
+            let backCard = document.querySelector(`#back${index}`);
+
+            // apply style of transform rotateY
+            frontCard.style.transform = 'rotateY(-180deg)';
+            backCard.style.transform = 'rotateY(0)';
+
+            if(this.firstImage === null) {
+
+                // take card image one of index
+                this.firstImage = index;
+
+            } else {
+
+                // take card image two of index
+                this.secondImage = index;
+
+                // match the card
+                this.matchCard();
+
+            }
+
+        },
+
+        // Function of matching card
+        matchCard() {
+
+            // take firstImage and secondImage backSrc as image match from cardData
+            const cardMatchOne = this.cardData[this.firstImage].backSrc;
+            const cardMatchTwo = this.cardData[this.secondImage].backSrc;
+
+            if(cardMatchOne === cardMatchTwo) {
+
+                // when all card are match
+                this.resetBoard();
+
+            } else {
+
+                // when card not match with image
+                setTimeout(() => {
+                    this.flipCard();
+                }, 300)
+
+            }
+
+        },
+
+        // Function of reset rotate
+        resetRotate() {
+
+            // take card one and two as dynamic id
+            let firstCardFront = document.querySelector(`#front${this.firstImage}`);
+            let firstCardBack = document.querySelector(`#back${this.firstImage}`);
+            let secondCardFront = document.querySelector(`#front${this.secondImage}`);
+            let secondCardBack = document.querySelector(`#back${this.secondImage}`);
+
+            // take card one and two apply style rotateY
+            firstCardFront.style.transform = 'rotateY(0)';
+            firstCardBack.style.transform = 'rotateY(180deg)';
+            secondCardFront.style.transform = 'rotateY(0)';
+            secondCardBack.style.transform = 'rotateY(180deg)';
+        },
+
+        // Function of shake card box
+        shakeCardBox() {
+
+            // take card one and two as dynamic id
+            let firstCardFront = document.querySelector(`#front${this.firstImage}`);
+            let firstCardBack = document.querySelector(`#back${this.firstImage}`);
+            let secondCardFront = document.querySelector(`#front${this.secondImage}`);
+            let secondCardBack = document.querySelector(`#back${this.secondImage}`);
+
+
+                // take card one and two apply class list card shake
+                firstCardFront.classList.add('card-shake');
+                firstCardBack.classList.add('card-shake');
+                secondCardFront.classList.add('card-shake');
+                secondCardBack.classList.add('card-shake');
+
+        },
+
+        // Function of flip card
+        flipCard() {
+            this.shakeCardBox();
+            setTimeout(() => {
+                this.resetRotate()
+                this.resetBoard();
+            }, 500);
+        },
+
+        // Function of reset board card
+        resetBoard() {
+
+            // take card one and two as dynamic id
+            let firstCardFront = document.querySelector(`#front${this.firstImage}`);
+            let firstCardBack = document.querySelector(`#back${this.firstImage}`);
+            let secondCardFront = document.querySelector(`#front${this.secondImage}`);
+            let secondCardBack = document.querySelector(`#back${this.secondImage}`);
+
+            // take card one and two apply class list card shake
+            firstCardFront.classList.remove('card-shake');
+            firstCardBack.classList.remove('card-shake');
+            secondCardFront.classList.remove('card-shake');
+            secondCardBack.classList.remove('card-shake');
+
+            this.firstImage = null;
+            this.secondImage = null;
+            this.lockBoard = false;
+        },
+
+    }
+}
+
+</script>
--- resources/js/spa/router/router.js
+++ resources/js/spa/router/router.js
@@ -3,6 +3,12 @@ import {createRouter, createWebHistory} from "vue-router";
 // === === === === Portal layout === === === === //
 import layout from "../layout/layout.vue";
 import home from "../pages/home.vue";
+import game from "../pages/game.vue";
+import profile from "../pages/profile.vue";
+import shop from "../pages/shop.vue";
+import settings from "../pages/settings.vue";
+import wallet from "../pages/wallet.vue";
+import milestone from "../pages/milestone.vue";
 
 // === === === === Routes Declare === === === === //
 const routes = [
@@ -12,6 +18,12 @@ const routes = [
         path: '/', name: 'layout', component: layout,
         children: [
             {path: '', name: 'home', component: home },
+            {path: 'game', name: 'game', component: game },
+            {path: 'profile', name: 'profile', component: profile },
+            {path: 'shop', name: 'shop', component: shop },
+            {path: 'settings', name: 'settings', component: settings },
+            {path: 'wallet', name: 'wallet', component: wallet },
+            {path: 'milestone', name: 'milestone', component: milestone },
         ]
     },
 
